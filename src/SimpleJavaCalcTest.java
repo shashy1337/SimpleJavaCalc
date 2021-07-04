b35018ef9e920ea53bc3bf559a2f6987e042db0a
shashy <igrok458@gmail.com>
2021-07-04 23:19:46 +0300

    fixes

--- src/SimpleJavaCalcTest.java
+++ src/SimpleJavaCalcTest.java
@@ -4,35 +4,28 @@ public class SimpleJavaCalcTest {
     public static void main(String[] args) {
         boolean cantStop = false;
         Scanner scan = new Scanner(System.in);
-        while (!cantStop){
+        int iterCounter = 0;
+
+        while (!cantStop) {
             int firstOperand = Integer.parseInt(String.valueOf(scan.nextInt()));
             char operation = scan.next().charAt(0);
             int secondOperand = Integer.parseInt(String.valueOf(scan.nextInt()));
-            int iterCounter = 0;
 
-            switch (operation){
-                case '*':
-                    System.out.println(SimpleJavaCalc.multiple(firstOperand, secondOperand));
-                    iterCounter += 1;
-                    break;
-                case '/':
-                    System.out.println(SimpleJavaCalc.division(firstOperand, secondOperand));
-                    iterCounter += 1;
-                    break;
-                case '-':
-                    System.out.println(SimpleJavaCalc.subtraction(firstOperand, secondOperand));
-                    iterCounter += 1;
-                    break;
-                case '+':
-                    System.out.println(SimpleJavaCalc.addition(firstOperand, secondOperand));
-                    iterCounter += 1;
-                    break;
+            System.out.println(iterCounter);
+            switch (operation) {
+                case '*' -> System.out.println(SimpleJavaCalc.multiple(firstOperand, secondOperand));
+                case '/' -> System.out.println(SimpleJavaCalc.division(firstOperand, secondOperand));
+                case '-' -> System.out.println(SimpleJavaCalc.subtraction(firstOperand, secondOperand));
+                case '+' -> System.out.println(SimpleJavaCalc.addition(firstOperand, secondOperand));
             }
 
-            if (iterCounter > 6){
-                System.out.println("Хотите ли вы звкрыть калькулятор? y/n: ");
+            iterCounter++;
+
+
+            if (iterCounter > 6) {
+                System.out.print("Хотите ли вы звкрыть калькулятор? y/n: ");
                 String scanString = scan.nextLine();
-                if (scanString == "Y"){
+                if (scanString == "Y") {
                     cantStop = false;
                 }
             } else {
